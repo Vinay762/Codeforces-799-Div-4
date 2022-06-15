ac7f8077d5f5870eb30a35836932db2e3aaab9b5
vinay762 <vinaytiwari1501@gmail.com>
2022-06-15 12:03:10 +0530

    Ola

---
+++ base.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+
+void solve(){
+    //check if there is test case
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t; cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
